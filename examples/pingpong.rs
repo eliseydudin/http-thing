51e9aae1e01fac12186e723e8bd712396b28a2b6
eliseydudin <dudinelisey630@gmail.com>
2025-01-28 19:57:16 +0300

    new example

---
+++ examples/pingpong.rs
@@ -0,0 +1,21 @@
+use clang_log::init;
+use http_router::prelude::*;
+
+struct PingPongRoute;
+
+impl Route for PingPongRoute {
+    const ACCEPTED_REQUEST: RequestType = RequestType::Get;
+    const PATH: &str = "/ping";
+
+    fn handler(&mut self) -> fn(Request) -> Response {
+        |_req| Response::new().body(b"pong")
+    }
+}
+
+fn main() {
+    init(log::Level::Trace, "pingpong");
+    let mut server = Server::new(8080, 20);
+    server.add_route(PingPongRoute);
+
+    server.run()
+}
